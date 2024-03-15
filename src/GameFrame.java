c27a8f0709d6c65a3cc395ee48b3c3bd4ee58f44
Omslice234 <144574186+Omslice234@users.noreply.github.com>
2024-03-15 00:56:46 -0400

    Add files via upload

---
+++ src/GameFrame.java
@@ -0,0 +1,24 @@
+package pongcsII2024;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+		GamePanel panel;
+		
+		GameFrame(){
+			panel = new GamePanel();
+			this.add(panel);
+			this.setTitle("Pong Game");
+			this.setResizable(false);
+			this.setBackground(Color.black);
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.pack();
+			this.setVisible(true);
+			this.setLocationRelativeTo(null);
+		}
+	}
+
---
+++ src/PongGame.java
@@ -0,0 +1,13 @@
+package pongcsII2024;
+
+public class PongGame {
+
+	public static void main(String[] args) {
+		
+
+							
+				GameFrame frame = new GameFrame();
+
+	}
+
+}
